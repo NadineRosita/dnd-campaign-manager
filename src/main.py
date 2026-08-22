4db41e70886eb63f9078a7b796938cb189c1f71f
NadineRoss <nadia.coronelcorrea@gmail.com>
2026-08-22 22:17:51 +0200

    feat: add character creation

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@
 from character import Character
 from character_manager import CharacterManager
 
+
 def display_menu() -> None:
     """Display the main menu."""
     print()
@@ -17,6 +18,31 @@ def display_menu() -> None:
     print("5. Exit")
     
 
+def create_character(manager: CharacterManager) -> None:
+    """Create a new character from user input."""
+    print("\n--- Create Character ---")
+    
+    name = input("Name: ")
+    race = input("Race: ")
+    character_class = input("Character class: ")
+    level = get_integer_input("Level: ")
+    hit_points = get_integer_input("Hit points: ")
+    
+    character = Character(name, race, character_class, level, hit_points)
+    
+    manager.add_character(character)
+    print(f"\nCharacter '{name} created successfully!")
+
+def get_integer_input(prompt: str) -> int:
+    """Ask the user for an integer until a valid value is provided."""
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+
+
 def main() -> None:
     """Run the application."""
     manager = CharacterManager()
@@ -26,7 +52,7 @@ def main() -> None:
         
         choice = input("\n Choose an option:")
         if choice == "1":
-            print("Create character")
+            create_character(manager)
         elif choice == "2":
             print("List characters")
         elif choice == "3":
